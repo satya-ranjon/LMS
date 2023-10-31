7846a9ce5f2e01375f4c38ed32c6a845affcf67b
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-10-31 21:51:46 +0600

    feat: Add HeroSection component for educational landing page
    This commit introduces the HeroSection component, which is part of the educational landing page. The HeroSection component features:
    
    - A visually appealing background with a hero pattern.
    - Prominent educational slogans and headings.
    - Two call-to-action buttons for user interaction.
    - An illustrative image to enhance the visual appeal.

--- src/pages/home/HeroSection.jsx
+++ src/pages/home/HeroSection.jsx
@@ -1,5 +1,40 @@
+import Button, { ButtonSize } from "../../components/common/Button";
+import Container from "../../components/common/Container";
+import { images } from "../../constant";
+
 const HeroSection = () => {
-  return <div>HeroSection</div>;
+  return (
+    <div
+      className={` w-full bg-no-repeat bg-cover  bg-center bg-hero-pattern  `}>
+      <Container>
+        <div className=" flex flex-col gap-10 md:gap-0 md:flex-row justify-between items-center py-24">
+          <div className="">
+            <div className=" flex flex-col gap-2 md:border-l-[3px] pl-5 border-l-lime-500">
+              <span className="text-center md:text-start text-light-1 text-3xl lg:text-5xl font-bold">
+                Education - Learning
+              </span>
+              <span className="text-center md:text-start text text-2xl lg:text-4xl font-bold  tracking-tight">
+                without limits!
+              </span>
+            </div>
+
+            <div className=" mt-10 flex flex-col md:flex-row  gap-4">
+              <Button size={ButtonSize.LG}>Join For Free</Button>
+              <Button size={ButtonSize.LG} outline={true}>
+                Login For LMS
+              </Button>
+            </div>
+          </div>
+          <div className="">
+            <img
+              src={images.certificate}
+              className="w-[340px] h-[300px] lg:w-[440px] lg:h-[480px] xl:w-[640px] xl:h-[580px]"
+            />
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default HeroSection;
