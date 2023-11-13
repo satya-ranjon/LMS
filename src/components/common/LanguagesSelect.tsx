0898a764a653c8781422b502081b293653d678b6
Satya Ranjon <satyaranjonofficial@gmail.com>
2023-11-13 21:58:58 +0600

    Refactor LanguagesSelect component
    The LanguagesSelect component has been refactored to introduce the option of displaying language icons. The Select component is now customized to support the rendering of icons and different text colors. This improves the reusability and visual flexibility of the LanguagesSelect component.

--- src/components/common/LanguagesSelect.tsx
+++ src/components/common/LanguagesSelect.tsx
@@ -1,14 +1,35 @@
+import LanguageIcon from "../ui/LanguageIcon";
 import Select from "./Select";
+import React from "react";
 
 const languages = [
   { value: "eng", label: "ENG" },
   { value: "bng", label: "BNG" },
 ];
-const LanguagesSelect = () => {
+
+interface LanguageProps {
+  showIcon?: boolean;
+}
+
+const LanguagesSelect: React.FC<LanguageProps> = ({ showIcon = false }) => {
   const handleSelectLanguage = (value: object) => {
     console.log("Language", value);
   };
-  return (
+  return showIcon ? (
+    <Select
+      defaultSelectedIndex={0}
+      options={languages}
+      onOptionSelected={handleSelectLanguage}
+      optionsColor="text-light-1"
+      renderLabel={({ getRecommendedLabelProps }) => {
+        return (
+          <button {...getRecommendedLabelProps()}>
+            <LanguageIcon />
+          </button>
+        );
+      }}
+    />
+  ) : (
     <Select
       defaultSelectedIndex={0}
       options={languages}
